dd76074aadf1e02fc94124a4faae1f84a05b7fd7
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-06-21 12:39:52 +0530

    Create all views and blog urlpatterns

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,49 @@
+from django.utils import timezone
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (DetailView, ListView, 
+                                  TemplateView, CreateView,
+                                  UpdateView, DeleteView)
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        # doing kind of a SQL query
+        # grab all the Post model objects and filter based on the conditions
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_draft_list.html'
+    model = Post
+
+    def get_queryset(self):
+        # grab all the posts which don't have a published date i.e. drafts
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
