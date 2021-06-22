ebbcdecb3749cc3520e568cbc227b28775fef07a
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-06-22 12:24:13 +0530

    Add site authentication stuff and login page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogs.urls'))
+    path('', include('blogs.urls')),
+    path('accounts/login/', views.LoginView.as_view(template='registration/login.html'), name='login'),
+    path('accounts/logout/',views.LogoutView.as_view(), name='logout', kwargs={'next_page':'/'})
 ]
